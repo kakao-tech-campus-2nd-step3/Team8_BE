61bfa776202e07cd821814042df396c791b1ff20
Seonghun Jeong <tjdgns5506@gmail.com>
2024-09-24 13:19:32 +0900

    Refactor: record 스타일 통일

--- src/main/java/com/example/sinitto/dto/CallbackResponse.java
+++ src/main/java/com/example/sinitto/dto/CallbackResponse.java
@@ -2,7 +2,8 @@ package com.example.sinitto.dto;
 
 import java.time.LocalDateTime;
 
-public record CallbackResponse(String seniorName,
-                               LocalDateTime postTime,
-                               String status) {
+public record CallbackResponse(
+        String seniorName,
+        LocalDateTime postTime,
+        String status) {
 }
