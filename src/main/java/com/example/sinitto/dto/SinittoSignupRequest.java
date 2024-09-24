7c3968831b034cc62df6d3f68221646694765181
Seonghun Jeong <tjdgns5506@gmail.com>
2024-09-24 13:00:34 +0900

    Refactor: MemberController관련 DTO들 record로 변경

--- src/main/java/com/example/sinitto/dto/SinittoSignupRequest.java
+++ src/main/java/com/example/sinitto/dto/SinittoSignupRequest.java
@@ -1,30 +1,9 @@
 package com.example.sinitto.dto;
 
-public class SinittoSignupRequest {
-
-    String name;
-    String phoneNumber;
-    String email;
-    String accountNumber;
-    String bankName;
-
-    public String getAccountNumber() {
-        return accountNumber;
-    }
-
-    public String getBankName() {
-        return bankName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
+public record SinittoSignupRequest(
+        String name,
+        String phoneNumber,
+        String email,
+        String accountNumber,
+        String bankName) {
 }
