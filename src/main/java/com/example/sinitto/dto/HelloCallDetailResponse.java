0b489594e6046d4016bb0921b5c1fdd99ab0a6c1
Seonghun Jeong <tjdgns5506@gmail.com>
2024-09-24 12:53:47 +0900

    Refactor: HelloCallController관련 DTO들 record로 변경

--- src/main/java/com/example/sinitto/dto/HelloCallDetailResponse.java
+++ src/main/java/com/example/sinitto/dto/HelloCallDetailResponse.java
@@ -1,41 +1,20 @@
 package com.example.sinitto.dto;
 
 import java.time.LocalTime;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-public class HelloCallDetailResponse {
+public record HelloCallDetailResponse(
+        Date startDate,
+        Date endDate,
+        List<TimeSlot> timeSlots,
+        String content
+) {
 
-    Date startDate;
-    Date endDate;
-    List<TimeSlot> timeSlots = new ArrayList<>();
-    String content;
-
-    public HelloCallDetailResponse() {
+    public record TimeSlot(
+            String day,
+            LocalTime startTime,
+            LocalTime endTime,
+            int serviceTime) {
     }
-
-    public Date getStartDate() {
-        return startDate;
-    }
-
-    public Date getEndDate() {
-        return endDate;
-    }
-
-    public List<TimeSlot> getTimeSlots() {
-        return timeSlots;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    static class TimeSlot {
-        String day;
-        LocalTime startTime;
-        LocalTime endTime;
-        int serviceTime;
-    }
-
 }
