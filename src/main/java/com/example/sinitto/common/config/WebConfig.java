670d2b6e103bab6c90caa2102fc2aa203afdd11f
Seonghun Jeong <119427233+zzoe2346@users.noreply.github.com>
2024-09-26 09:28:51 +0900

    Feat: MemberId위한 MethodArgumentResolver 구현 (#13)
    - MemberId Annotation 구현
    - MemberIdArgumentResolver class 구현
    - WebConfig class 구현 후 MemberIdArgumentResolver 세팅

---
+++ src/main/java/com/example/sinitto/common/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.example.sinitto.common.config;
+
+import com.example.sinitto.common.resolver.MemberIdArgumentResolver;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new MemberIdArgumentResolver());
+    }
+
+}
