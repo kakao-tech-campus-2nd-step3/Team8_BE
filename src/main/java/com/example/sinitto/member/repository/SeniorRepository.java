94b12586a64a19a28397219453bd523eeeca7a34
Seonghun Jeong <tjdgns5506@gmail.com>
2024-09-29 21:50:12 +0900

    Feat: SeniorRepository 생성
    - 테스트할 때 save()가 필요해 생성
    - 시니어 추가 기능 만들때 어차피 추가 되는것으로 판단

---
+++ src/main/java/com/example/sinitto/member/repository/SeniorRepository.java
@@ -0,0 +1,7 @@
+package com.example.sinitto.member.repository;
+
+import com.example.sinitto.member.entity.Senior;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SeniorRepository extends JpaRepository<Senior, Long> {
+}
