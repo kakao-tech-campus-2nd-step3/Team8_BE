f9812022f4f181becec5886fdd211e4090d9e12a
gitjiho <jiho99322@gmail.com>
2024-09-29 18:27:51 +0900

    fix: jwt.secret 값을 dev 속성으로 위치 변경 및 KakaoProperties frontendUri 필드 삭제

--- src/main/java/com/example/sinitto/common/properties/KakaoProperties.java
+++ src/main/java/com/example/sinitto/common/properties/KakaoProperties.java
@@ -5,7 +5,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix = "kakao")
 public record KakaoProperties(
         String clientId,
-        String redirectUri,
-        String frontRedirectUri
+        String redirectUri
 ) {
 }
