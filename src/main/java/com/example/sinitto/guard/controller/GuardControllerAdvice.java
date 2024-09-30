7508b22be0363eab53213b687c6c146be6c244b5
2iedo <162278702+2iedo@users.noreply.github.com>
2024-09-30 16:53:13 +0900

    refactor : responseEntity 리턴하도록 변경

--- src/main/java/com/example/sinitto/guard/controller/GuardControllerAdvice.java
+++ src/main/java/com/example/sinitto/guard/controller/GuardControllerAdvice.java
@@ -3,6 +3,7 @@ package com.example.sinitto.guard.controller;
 import com.example.sinitto.guard.exception.SeniorNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -11,11 +12,11 @@ import java.net.URI;
 @RestControllerAdvice(basePackages = "com.example.sinitto.guard")
 public class GuardControllerAdvice {
     @ExceptionHandler(SeniorNotFoundException.class)
-    public ProblemDetail handleSeniorNotFoundException(SeniorNotFoundException e){
+    public ResponseEntity<ProblemDetail> handleSeniorNotFoundException(SeniorNotFoundException e){
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, e.getMessage());
         problemDetail.setType(URI.create("/error/senior-not-found"));
         problemDetail.setTitle("Senior Not Found");
 
-        return problemDetail;
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(problemDetail);
     }
 }
