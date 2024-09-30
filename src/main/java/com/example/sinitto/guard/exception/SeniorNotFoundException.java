55b952574cb35109e45afd53e8e140bcb53d0a6e
2iedo <162278702+2iedo@users.noreply.github.com>
2024-09-30 15:25:21 +0900

    feat : SeniorNotFoundException 클래스 추가

---
+++ src/main/java/com/example/sinitto/guard/exception/SeniorNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.sinitto.guard.exception;
+
+public class SeniorNotFoundException extends RuntimeException {
+    public SeniorNotFoundException(String message) {
+        super(message);
+    }
+}
