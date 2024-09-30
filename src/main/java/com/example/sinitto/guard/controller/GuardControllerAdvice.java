ab552746986178bfe9f4012cfc9b4d12fb05f734
2iedo <162278702+2iedo@users.noreply.github.com>
2024-09-30 15:26:16 +0900

    feat : GuardControllerAdvice 클래스 추가

---
+++ src/main/java/com/example/sinitto/guard/controller/GuardControllerAdvice.java
@@ -0,0 +1,21 @@
+package com.example.sinitto.guard.controller;
+
+import com.example.sinitto.guard.exception.SeniorNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.net.URI;
+
+@RestControllerAdvice
+public class GuardControllerAdvice {
+    @ExceptionHandler(SeniorNotFoundException.class)
+    public ProblemDetail handleSeniorNotFoundException(SeniorNotFoundException e){
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, e.getMessage());
+        problemDetail.setType(URI.create("/error/senior-not-found"));
+        problemDetail.setTitle("Senior Not Found");
+
+        return problemDetail;
+    }
+}
