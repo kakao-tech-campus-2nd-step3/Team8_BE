115c880e59c5f5f3de57e2fcad9c01e63bbf6972
2iedo <162278702+2iedo@users.noreply.github.com>
2024-09-30 16:45:59 +0900

    feat : findByIsSinitto 메소드 추가 모든 보호자 불러오는데 사용

--- src/main/java/com/example/sinitto/member/repository/MemberRepository.java
+++ src/main/java/com/example/sinitto/member/repository/MemberRepository.java
@@ -4,12 +4,13 @@ import com.example.sinitto.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Optional<Member> findByEmail(String email);
-
     boolean existsByEmail(String email);
+    List<Member> findByIsSinitto(boolean isSinitto);
 }
