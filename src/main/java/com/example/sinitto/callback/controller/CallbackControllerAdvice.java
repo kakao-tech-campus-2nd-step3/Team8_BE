b61cd339fee778c906e558075ce1520a31175d07
Seonghun Jeong <tjdgns5506@gmail.com>
2024-10-01 17:47:26 +0900

    Feat: 더 디테일한 에러 응답을 위해 `ResponseEntity` 적용

--- src/main/java/com/example/sinitto/callback/controller/CallbackControllerAdvice.java
+++ src/main/java/com/example/sinitto/callback/controller/CallbackControllerAdvice.java
@@ -5,28 +5,40 @@ import com.example.sinitto.callback.exception.ForbiddenException;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.net.URI;
 
 @RestControllerAdvice(basePackages = "com.example.sinitto.callback")
 public class CallbackControllerAdvice {
 
     @ExceptionHandler(EntityNotFoundException.class)
-    public ProblemDetail handleEntityNotFoundException(EntityNotFoundException e) {
+    public ResponseEntity<ProblemDetail> handleEntityNotFoundException(EntityNotFoundException e) {
 
-        return ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, e.getMessage());
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, e.getMessage());
+        problemDetail.setType(URI.create("/errors/bad-request"));
+        problemDetail.setTitle("Bad Request");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(problemDetail);
     }
 
     @ExceptionHandler(ForbiddenException.class)
-    public ProblemDetail handleForbiddenException(ForbiddenException e) {
+    public ResponseEntity<ProblemDetail> handleForbiddenException(ForbiddenException e) {
 
-        return ProblemDetail.forStatusAndDetail(HttpStatus.FORBIDDEN, e.getMessage());
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.FORBIDDEN, e.getMessage());
+        problemDetail.setType(URI.create("/errors/forbidden"));
+        problemDetail.setTitle("Forbidden");
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(problemDetail);
     }
 
     @ExceptionHandler(ConflictException.class)
-    public ProblemDetail handleConflictException(ConflictException e) {
+    public ResponseEntity<ProblemDetail> handleConflictException(ConflictException e) {
 
-        return ProblemDetail.forStatusAndDetail(HttpStatus.CONFLICT, e.getMessage());
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.CONFLICT, e.getMessage());
+        problemDetail.setType(URI.create("/errors/conflict"));
+        problemDetail.setTitle("Conflict");
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(problemDetail);
     }
 
 }
