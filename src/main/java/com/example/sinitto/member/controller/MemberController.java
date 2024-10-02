e7ec7d3672e67ba45d11e9ec08a2d4807c844c2d
Seonghun Jeong <tjdgns5506@gmail.com>
2024-10-02 20:13:24 +0900

    Feat: Member, Auth 도메인 에서는 JWT 제외 시키기 위한 내용 추가
    - 전역으로 `SecurityRequirement().addList("bearerAuth")`를 설정해놓음. 이를 피하기 위한 `security = @SecurityRequirement(name = "JWT제외")`

--- src/main/java/com/example/sinitto/member/controller/MemberController.java
+++ src/main/java/com/example/sinitto/member/controller/MemberController.java
@@ -4,6 +4,7 @@ import com.example.sinitto.auth.dto.RegisterResponse;
 import com.example.sinitto.member.dto.SignupRequest;
 import com.example.sinitto.member.service.MemberService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,7 +24,7 @@ public class MemberController {
         this.memberService = memberService;
     }
 
-    @Operation(summary = "시니또 회원가입", description = "시니또의 회원가입")
+    @Operation(summary = "시니또 회원가입", description = "시니또의 회원가입", security = @SecurityRequirement(name = "JWT제외"))
     @PostMapping("/sinitto")
     public ResponseEntity<RegisterResponse> sinittoSignup(@RequestBody SignupRequest request) {
         RegisterResponse registerResponse = memberService.registerNewMember(request.name(), request.phoneNumber(),
@@ -31,7 +32,7 @@ public class MemberController {
         return ResponseEntity.status(HttpStatus.CREATED).body(registerResponse);
     }
 
-    @Operation(summary = "보호자 회원가입", description = "보호자의 회원가입")
+    @Operation(summary = "보호자 회원가입", description = "보호자의 회원가입", security = @SecurityRequirement(name = "JWT제외"))
     @PostMapping("/guard")
     public ResponseEntity<RegisterResponse> guardSignup(@RequestBody SignupRequest request) {
         RegisterResponse registerResponse = memberService.registerNewMember(request.name(), request.phoneNumber(),
