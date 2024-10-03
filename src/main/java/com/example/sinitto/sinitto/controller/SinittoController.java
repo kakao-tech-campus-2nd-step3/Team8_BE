222b713ddb67552b1dff911c6e385f107a4f3608
eunsoni <esk12704@gmail.com>
2024-10-03 22:30:52 +0900

    feat: 시니또 계좌정보 관련 api 추가

--- src/main/java/com/example/sinitto/sinitto/controller/SinittoController.java
+++ src/main/java/com/example/sinitto/sinitto/controller/SinittoController.java
@@ -1,6 +1,7 @@
 package com.example.sinitto.sinitto.controller;
 
 import com.example.sinitto.common.annotation.MemberId;
+import com.example.sinitto.guard.dto.SeniorRequest;
 import com.example.sinitto.sinitto.dto.SinittoRequest;
 import com.example.sinitto.sinitto.dto.SinittoResponse;
 import com.example.sinitto.sinitto.service.SinittoService;
@@ -28,14 +29,27 @@ public class SinittoController {
         return ResponseEntity.ok(sinittoService.readSinitto(memberId));
     }
 
+    @Operation(summary = "계좌정보 등록", description = "시니또가 계좌정보 등록합니다.")
+    @PostMapping("/bank")
+    public ResponseEntity<String> createSenior(@MemberId Long memberId, @RequestBody SinittoRequest sinittoRequest) {
+        sinittoService.createSinittoBankInfo(memberId, sinittoRequest);
+        return ResponseEntity.ok("계좌등록되었습니다.");
+    }
+
     @Operation(summary = "시니또 본인 정보 수정", description = "시니또 본인의 정보를 수정요청한다.")
     @PutMapping
-    public ResponseEntity<String> updateSinittoInfo(@MemberId Long memberId, @RequestBody SinittoRequest Sinittorequest) {
+    public ResponseEntity<String> updateSinitto(@MemberId Long memberId, @RequestBody SinittoRequest Sinittorequest) {
+        sinittoService.updateSinitto(memberId, Sinittorequest);
+        return ResponseEntity.ok("시니또 정보가 수정되었습니다.");
+    }
+
+    @Operation(summary = "시니또 계좌 정보 수정", description = "시니또 본인의 정보를 수정요청한다.")
+    @PutMapping("/bank")
+    public ResponseEntity<String> updateSinittoBankInfo(@MemberId Long memberId, @RequestBody SinittoRequest Sinittorequest) {
         sinittoService.updateSinittoBankInfo(memberId, Sinittorequest);
         return ResponseEntity.ok("시니또 정보가 수정되었습니다.");
     }
 
-    //Pass
     @Operation(summary = "시니또 삭제", description = "관리자용")
     @DeleteMapping
     public ResponseEntity<String> deleteSinitto(@MemberId Long memberId) {
@@ -43,7 +57,13 @@ public class SinittoController {
         return ResponseEntity.ok("시니또가 삭제되었습니다.");
     }
 
-    //Pass
+    @Operation(summary = "시니또 계좌정보 삭제", description = "관리자용")
+    @DeleteMapping("/bank")
+    public ResponseEntity<String> deleteSinittoBankInfo(@MemberId Long memberId) {
+        sinittoService.deleteSinittoBankInfo(memberId);
+        return ResponseEntity.ok("시니또 계좌정보가 삭제되었습니다.");
+    }
+
     @Operation(summary = "모든 시니또 조회", description = "관리자용")
     @GetMapping("/all")
     public ResponseEntity<List<SinittoResponse>> getAllSinittos() {
