44e6428b9582331defbd4a00ccfa498c61741937
eunsoni <esk12704@gmail.com>
2024-10-09 19:22:27 +0900

    feat: GuardGuidelineRepository 추가

---
+++ src/main/java/com/example/sinitto/guardGuideline/repository/GuardGuidelineRepository.java
@@ -0,0 +1,7 @@
+package com.example.sinitto.guardGuideline.repository;
+
+import com.example.sinitto.guardGuideline.entity.GuardGuideline;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GuardGuidelineRepository extends JpaRepository<GuardGuideline, Long> {
+}
