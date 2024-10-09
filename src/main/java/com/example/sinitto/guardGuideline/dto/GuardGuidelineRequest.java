6953b56ab31499659fbdecdea3b9986ef9cbe64c
eunsoni <esk12704@gmail.com>
2024-10-09 18:57:51 +0900

    feat: 가이드라인 dto(request) 추가

--- src/main/java/com/example/sinitto/guardGuideline/dto/GuardGuidelineRequest.java
+++ src/main/java/com/example/sinitto/guardGuideline/dto/GuardGuidelineRequest.java
@@ -1,4 +1,8 @@
 package com.example.sinitto.guardGuideline.dto;
 
-public record GuardGuidelineRequest() {
-}
+public record GuardGuidelineRequest(
+        Long seniorId,
+        String type,
+        String title,
+        String content
+) {}
