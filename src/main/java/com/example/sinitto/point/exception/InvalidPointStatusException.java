7c98122353c3d1844e6bef2898abec5b5420297c
Seonghun Jeong <tjdgns5506@gmail.com>
2024-10-11 15:55:22 +0900

    fix: 머지후 수정사항

---
+++ src/main/java/com/example/sinitto/point/exception/InvalidPointStatusException.java
@@ -0,0 +1,8 @@
+package com.example.sinitto.point.exception;
+
+public class InvalidPointStatusException extends RuntimeException {
+
+    public InvalidPointStatusException(String message) {
+        super(message);
+    }
+}
