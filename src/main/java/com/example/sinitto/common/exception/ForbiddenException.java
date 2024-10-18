b1b53c9546e17ef8c59e6fb2bdb250932f2adf26
Seonghun Jeong <tjdgns5506@gmail.com>
2024-10-18 20:59:36 +0900

    refactor: 콜백 도메인 예외 처리 리팩토링

---
+++ src/main/java/com/example/sinitto/common/exception/ForbiddenException.java
@@ -0,0 +1,8 @@
+package com.example.sinitto.common.exception;
+
+public class ForbiddenException extends RuntimeException {
+
+    public ForbiddenException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/example/sinitto/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/sinitto/common/exception/GlobalExceptionHandler.java
@@ -1,4 +1,4 @@
-package com.example.sinitto.callback.controller;
+package com.example.sinitto.common.exception;
 
 import com.example.sinitto.callback.exception.ConflictException;
 import com.example.sinitto.callback.exception.ForbiddenException;
@@ -9,16 +9,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.net.URI;
-
-@RestControllerAdvice(basePackages = "com.example.sinitto.callback")
-public class CallbackControllerAdvice {
+@RestControllerAdvice
+public class GlobalExceptionHandler {
 
     @ExceptionHandler(ForbiddenException.class)
     public ResponseEntity<ProblemDetail> handleForbiddenException(ForbiddenException e) {
 
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.FORBIDDEN, e.getMessage());
-        problemDetail.setType(URI.create("/errors/forbidden"));
         problemDetail.setTitle("Forbidden");
         return ResponseEntity.status(HttpStatus.FORBIDDEN).body(problemDetail);
     }
@@ -27,7 +24,6 @@ public class CallbackControllerAdvice {
     public ResponseEntity<ProblemDetail> handleConflictException(ConflictException e) {
 
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.CONFLICT, e.getMessage());
-        problemDetail.setType(URI.create("/errors/conflict"));
         problemDetail.setTitle("Conflict");
         return ResponseEntity.status(HttpStatus.CONFLICT).body(problemDetail);
     }
@@ -36,7 +32,6 @@ public class CallbackControllerAdvice {
     public ResponseEntity<ProblemDetail> handleNotFoundException(NotFoundException e) {
 
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, e.getMessage());
-        problemDetail.setType(URI.create("/errors/not-found"));
         problemDetail.setTitle("Not Found");
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(problemDetail);
     }
