ca76a5cf255a6086a1083d681deb93df6fffd22f
eunsoni <135586807+eunsoni@users.noreply.github.com>
2024-10-23 15:17:18 +0900

    Refactor: 가이드라인 관련 로직 수정 {프론트 요청) (#103)
    * deploy: 가이드라인 더미데이터 추가
    
    * feat: 콜백과 가이드라인의 시니어가 같지 않을 때 발생하는 예외 구현
    
    * refactor: 타입별 전체 가이드라인 및 특정 가이드라인 조회 시 검증 로직 추가
    
    * refactor: 가이드라인 조회 api 매개변수 수정
    
    * refactor: Reformat code
    
    * refactor: 가이드라인 response dto에 id 추가 및 서비스단 수정
    
    * refactor: 중복 주석 삭제
    
    * refactor: 서비스단에서 사용하지 않는 레퍼지토리 선언 삭제

--- src/main/java/com/example/sinitto/guardGuideline/service/GuardGuidelineService.java
+++ src/main/java/com/example/sinitto/guardGuideline/service/GuardGuidelineService.java
@@ -1,6 +1,9 @@
 package com.example.sinitto.guardGuideline.service;
 
 
+import com.example.sinitto.callback.entity.Callback;
+import com.example.sinitto.callback.exception.NotExistCallbackException;
+import com.example.sinitto.callback.repository.CallbackRepository;
 import com.example.sinitto.guard.exception.SeniorNotFoundException;
 import com.example.sinitto.guard.repository.SeniorRepository;
 import com.example.sinitto.guardGuideline.dto.GuardGuidelineRequest;
@@ -8,6 +11,7 @@ import com.example.sinitto.guardGuideline.dto.GuardGuidelineResponse;
 import com.example.sinitto.guardGuideline.entity.GuardGuideline;
 import com.example.sinitto.guardGuideline.entity.GuardGuideline.Type;
 import com.example.sinitto.guardGuideline.exception.GuardGuidelineNotFoundException;
+import com.example.sinitto.guardGuideline.exception.SeniorAndGuardGuidelineMismatchException;
 import com.example.sinitto.guardGuideline.exception.SeniorAndGuardMemberMismatchException;
 import com.example.sinitto.guardGuideline.repository.GuardGuidelineRepository;
 import com.example.sinitto.member.entity.Senior;
@@ -21,10 +25,12 @@ public class GuardGuidelineService {
 
     private final GuardGuidelineRepository guardGuidelineRepository;
     private final SeniorRepository seniorRepository;
+    private final CallbackRepository callbackRepository;
 
-    public GuardGuidelineService (GuardGuidelineRepository guardGuidelineRepository, SeniorRepository seniorRepository){
+    public GuardGuidelineService(GuardGuidelineRepository guardGuidelineRepository, SeniorRepository seniorRepository, CallbackRepository callbackRepository) {
         this.guardGuidelineRepository = guardGuidelineRepository;
         this.seniorRepository = seniorRepository;
+        this.callbackRepository = callbackRepository;
     }
 
     @Transactional
@@ -40,18 +46,18 @@ public class GuardGuidelineService {
     }
 
     @Transactional(readOnly = true)
-    public List<GuardGuidelineResponse> readAllGuardGuidelinesByCategory(Long seniorId, Type type){
+    public List<GuardGuidelineResponse> readAllGuardGuidelinesByCategory(Long seniorId, Type type) {
         List<GuardGuideline> guardGuidelines = guardGuidelineRepository.findBySeniorIdAndType(seniorId, type);
 
         return guardGuidelines.stream()
-                .map(m -> new GuardGuidelineResponse(m.getType(), m.getTitle(), m.getContent()))
+                .map(m -> new GuardGuidelineResponse(m.getId(), m.getType(), m.getTitle(), m.getContent()))
                 .toList();
     }
 
     @Transactional
     public void updateGuardGuideline(Long memberId, Long guidelineId, GuardGuidelineRequest guardGuidelineRequest) {
         GuardGuideline guardGuideline = guardGuidelineRepository.findById(guidelineId).orElseThrow(
-                ()-> new GuardGuidelineNotFoundException("해당 가이드라인이 존재하지 않습니다.")
+                () -> new GuardGuidelineNotFoundException("해당 가이드라인이 존재하지 않습니다.")
         );
 
         Senior senior = seniorRepository.findById(guardGuidelineRequest.seniorId()).orElseThrow(
@@ -66,21 +72,32 @@ public class GuardGuidelineService {
     }
 
     @Transactional(readOnly = true)
-    public List<GuardGuidelineResponse> readAllGuardGuidelinesBySenior(Long seniorId){
+    public List<GuardGuidelineResponse> readAllGuardGuidelinesBySenior(Long memberId, Long seniorId) {
+        Senior senior = seniorRepository.findById(seniorId).orElseThrow(
+                () -> new SeniorNotFoundException("시니어를 찾을 수 없습니다.")
+        );
+        if (senior.isNotGuard(memberId)) {
+            throw new SeniorAndGuardMemberMismatchException("해당 Guard의 Senior가 아닙니다.");
+        }
         List<GuardGuideline> guardGuidelines = guardGuidelineRepository.findBySeniorId(seniorId);
-
         return guardGuidelines.stream()
-                .map(m -> new GuardGuidelineResponse(m.getType(), m.getTitle(), m.getContent()))
+                .map(m -> new GuardGuidelineResponse(m.getId(), m.getType(), m.getTitle(), m.getContent()))
                 .toList();
     }
 
     @Transactional(readOnly = true)
-    public GuardGuidelineResponse readGuardGuideline(Long guidelineId){
+    public GuardGuidelineResponse readGuardGuideline(Long callbackId, Long guidelineId) {
+
+        Callback callback = callbackRepository.findById(callbackId)
+                .orElseThrow(() -> new NotExistCallbackException("존재하지 않는 콜백입니다"));
+
         GuardGuideline guardGuideline = guardGuidelineRepository.findById(guidelineId).orElseThrow(
-                ()-> new GuardGuidelineNotFoundException("해당 가이드라인이 존재하지 않습니다.")
-        );
+                () -> new GuardGuidelineNotFoundException("해당 가이드라인이 존재하지 않습니다."));
 
-        return new GuardGuidelineResponse(guardGuideline.getType(), guardGuideline.getTitle(), guardGuideline.getContent());
+        if (!callback.getSenior().equals(guardGuideline.getSenior())) {
+            throw new SeniorAndGuardGuidelineMismatchException("해당 Senior의 가이드라인이 아닙니다.");
+        }
+
+        return new GuardGuidelineResponse(guardGuideline.getId(), guardGuideline.getType(), guardGuideline.getTitle(), guardGuideline.getContent());
     }
-
 }
