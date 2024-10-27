f4a80749243b8b40d305d405fba751a7d644909b
eunsoni <esk12704@gmail.com>
2024-10-27 15:54:23 +0900

    refactor: 시니또용 카테고리별 가이드라인 조회 api 추가

--- src/main/java/com/example/sinitto/guardGuideline/controller/GuardGuidelineController.java
+++ src/main/java/com/example/sinitto/guardGuideline/controller/GuardGuidelineController.java
@@ -30,10 +30,16 @@ public class GuardGuidelineController {
         return ResponseEntity.ok("가이드라인이 추가되었습니다.");
     }
 
-    @Operation(summary = "카테고리에 해당하는 모든 가이드라인 조회", description = "시니또용 앱에서 카테고리에 해당하는 모든 가이드라인들을 요청할 때 필요합니다.")
-    @GetMapping("/{seniorId}/{type}")
-    public ResponseEntity<List<GuardGuidelineResponse>> getGuardGuidelinesByCategory(@PathVariable Long seniorId, @PathVariable GuardGuideline.Type type) {
-        return ResponseEntity.ok(guardGuidelineService.readAllGuardGuidelinesByCategory(seniorId, type));
+    @Operation(summary = "카테고리에 해당하는 모든 가이드라인 조회(보호자용)", description = "보호자용 앱에서 카테고리에 해당하는 모든 가이드라인들을 요청할 때 필요합니다.")
+    @GetMapping("/guard/{seniorId}/{type}")
+    public ResponseEntity<List<GuardGuidelineResponse>> getGuardGuidelinesByCategoryAndSenior(@MemberId Long memberId, @PathVariable Long seniorId, @PathVariable GuardGuideline.Type type) {
+        return ResponseEntity.ok(guardGuidelineService.readAllGuardGuidelinesByCategoryAndSenior(memberId, seniorId, type));
+    }
+
+    @Operation(summary = "카테고리에 해당하는 모든 가이드라인 조회(시니또용)", description = "시니또용 앱에서 카테고리에 해당하는 모든 가이드라인들을 요청할 때 필요합니다.")
+    @GetMapping("/sinitto/{callbackId}/{type}")
+    public ResponseEntity<List<GuardGuidelineResponse>> getGuardGuidelinesByCategoryAndCallback(@PathVariable Long callbackId, @PathVariable GuardGuideline.Type type) {
+        return ResponseEntity.ok(guardGuidelineService.readAllGuardGuidelinesByCategoryAndCallback(callbackId, type));
     }
 
     @Operation(summary = "가이드라인 수정", description = "보호자가 특정 가이드라인을 수정할 때 필요합니다.")
@@ -50,11 +56,6 @@ public class GuardGuidelineController {
         return ResponseEntity.ok(guardGuidelineService.readAllGuardGuidelinesBySenior(memberId, seniorId));
     }
 
-    @Operation(summary = "특정 가이드라인 조회", description = "보호자용 API입니다.")
-    @GetMapping("/detail")
-    public ResponseEntity<GuardGuidelineResponse> getGuardGuideline(@RequestParam("callbackId") Long callbackId, @RequestParam("guidelineId") Long guidelineId) {
-        return ResponseEntity.ok(guardGuidelineService.readGuardGuideline(callbackId, guidelineId));
-    }
 
     @Operation(summary = "특정 가이드라인 삭제", description = "보호자용 API입니다.")
     @DeleteMapping("/delete")
