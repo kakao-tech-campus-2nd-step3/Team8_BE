00db43306c2dc2a805f1d031a2a83014b5f6a8fc
2iedo <162278702+2iedo@users.noreply.github.com>
2024-11-06 02:03:35 +0900

    feat, hotfix : 회원탈퇴 api 추가 및 redisTemplate 수정

--- src/main/java/com/example/sinitto/member/controller/MemberController.java
+++ src/main/java/com/example/sinitto/member/controller/MemberController.java
@@ -44,4 +44,10 @@ public class MemberController {
         memberService.memberLogout(memberId);
         return ResponseEntity.ok().build();
     }
+
+    @Operation(summary = "멤버 회원탈퇴", description = "회원 정보를 삭제합니다.")
+    public ResponseEntity<Void> deleteMember(@MemberId Long memberId) {
+        memberService.deleteMember(memberId);
+        return ResponseEntity.ok().build();
+    }
 }
