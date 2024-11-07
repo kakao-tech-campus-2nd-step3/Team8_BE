a63335c49b4799a6485deb7978d5c7dced3b25bc
Seonghun Jeong <tjdgns5506@gmail.com>
2024-11-07 15:22:12 +0900

    fix: 개별 트랜잭션 적용. 기존에는 개별이 아니라 하나의 트랜잭션에 포함되어 있었다.

--- src/main/java/com/example/sinitto/callback/service/CallbackService.java
+++ src/main/java/com/example/sinitto/callback/service/CallbackService.java
@@ -19,6 +19,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
@@ -110,7 +111,7 @@ public class CallbackService {
         }
     }
 
-    @Transactional
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void completeCallbackIndividually(Callback callback) {
 
         pointService.earnPoint(callback.getAssignedMemberId(), CALLBACK_PRICE, PointLog.Content.COMPLETE_CALLBACK_AND_EARN);
