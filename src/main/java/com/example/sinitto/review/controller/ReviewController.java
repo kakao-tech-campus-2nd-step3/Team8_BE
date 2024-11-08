40b151a79ea307300a3d748a4b42eea69e83c107
2iedo <162278702+2iedo@users.noreply.github.com>
2024-11-08 18:12:06 +0900

    refactor : 리뷰 조회 경로 변경

--- src/main/java/com/example/sinitto/review/controller/ReviewController.java
+++ src/main/java/com/example/sinitto/review/controller/ReviewController.java
@@ -1,6 +1,5 @@
 package com.example.sinitto.review.controller;
 
-import com.example.sinitto.common.annotation.MemberId;
 import com.example.sinitto.review.dto.ReviewRequest;
 import com.example.sinitto.review.dto.ReviewResponse;
 import com.example.sinitto.review.service.ReviewService;
@@ -23,14 +22,14 @@ public class ReviewController {
     }
 
     @Operation(summary = "서비스 리뷰 리스트 보기", description = "서비스 리뷰를 리스트 형태로 보여줍니다.")
-    @GetMapping
+    @GetMapping("/all")
     public ResponseEntity<List<ReviewResponse>> getReviewList() {
         return ResponseEntity.ok(reviewService.readAllReviews());
     }
 
     @Operation(summary = "서비스 리뷰 및 평가 제출", description = "시니또에 대한 별점과 작성한 평가 내용(선택사항)을 제출합니다.")
     @PostMapping
-    public ResponseEntity<String> submitReview(@MemberId Long memberId, @RequestBody ReviewRequest reviewRequest) {
+    public ResponseEntity<String> submitReview(@RequestAttribute("memberId") Long memberId, @RequestBody ReviewRequest reviewRequest) {
         reviewService.createReview(memberId, reviewRequest);
         return ResponseEntity.ok("리뷰가 성공적으로 제출되었습니다.");
     }
