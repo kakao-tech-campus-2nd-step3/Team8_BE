b9bb7d7e43beb1464836f9865921af67497ed732
Seonghun Jeong <tjdgns5506@gmail.com>
2024-11-08 21:39:52 +0900

    refactor: String -> Content

--- src/main/java/com/example/sinitto/point/entity/PointLog.java
+++ src/main/java/com/example/sinitto/point/entity/PointLog.java
@@ -21,7 +21,7 @@ public class PointLog {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @NotNull
-    private String content;
+    private PointLog.Content content;
     @NotNull
     private int price;
     @CreatedDate
@@ -36,7 +36,7 @@ public class PointLog {
     private Member member;
 
 
-    public PointLog(String content, Member member, int price, Status status) {
+    public PointLog(Content content, Member member, int price, Status status) {
         this.content = content;
         this.member = member;
         this.price = price;
@@ -120,7 +120,7 @@ public class PointLog {
         return status;
     }
 
-    public String getContent() {
+    public Content getContent() {
         return content;
     }
 
@@ -145,7 +145,8 @@ public class PointLog {
         SPEND_COMPLETE_HELLO_CALL("안부전화 신청"),
         SPEND_CANCEL_HELLO_CALL("안부전화 신청 취소"),
         CHARGE_REQUEST("포인트 충전"),
-        WITHDRAW_REQUEST("포인트 출금");
+        WITHDRAW_REQUEST("포인트 출금"),
+        WELCOME_POINT("환영 포인트");
 
         private final String message;
 
