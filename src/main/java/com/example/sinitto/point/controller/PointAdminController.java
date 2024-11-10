30d8d238acb8645692bba0f8438fac8538137d28
eunsoni <esk12704@gmail.com>
2024-11-11 06:39:47 +0900

    feat: 포인트 출금, 충전 api 및 페이지 접근 시 세션정보 인증 로직 추가

--- src/main/java/com/example/sinitto/point/controller/PointAdminController.java
+++ src/main/java/com/example/sinitto/point/controller/PointAdminController.java
@@ -3,6 +3,7 @@ package com.example.sinitto.point.controller;
 import com.example.sinitto.point.dto.PointLogWithBankInfo;
 import com.example.sinitto.point.dto.PointLogWithDepositMessage;
 import com.example.sinitto.point.service.PointAdminService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,8 +22,10 @@ public class PointAdminController {
     }
 
     @GetMapping("/admin/point/charge")
-    public String showAllChargeRequest(Model model) {
-
+    public String showAllChargeRequest(Model model, HttpSession session) {
+        if(!isAdmin(session)){
+            return "redirect:/admin/login";
+        }
         List<PointLogWithDepositMessage> logWithDepositMessages = pointAdminService.getPointLogWithDepositMessage();
 
         model.addAttribute("logWithDepositMessages", logWithDepositMessages);
@@ -31,28 +34,37 @@ public class PointAdminController {
     }
 
     @PostMapping("/admin/point/charge/waiting/{pointLogId}")
-    public String changeToWaiting(@PathVariable Long pointLogId) {
-
+    public String changeToWaiting(@PathVariable Long pointLogId, HttpSession session) {
+        if(!isAdmin(session)){
+            return "redirect:/admin/login";
+        }
         pointAdminService.changeChargeLogToWaiting(pointLogId);
         return "redirect:/admin/point/charge";
     }
 
     @PostMapping("/admin/point/charge/complete/{pointLogId}")
-    public String changeToCompleteAndEarn(@PathVariable Long pointLogId) {
-
+    public String changeToCompleteAndEarn(@PathVariable Long pointLogId, HttpSession session) {
+        if(!isAdmin(session)){
+            return "redirect:/admin/login";
+        }
         pointAdminService.earnPointAndChangeToChargeComplete(pointLogId);
         return "redirect:/admin/point/charge";
     }
 
     @PostMapping("/admin/point/charge/fail/{pointLogId}")
-    public String changeToFail(@PathVariable Long pointLogId) {
-
+    public String changeToFail(@PathVariable Long pointLogId, HttpSession session) {
+        if(!isAdmin(session)){
+            return "redirect:/admin/login";
+        }
         pointAdminService.changeChargeLogToFail(pointLogId);
         return "redirect:/admin/point/charge";
     }
 
     @GetMapping("/admin/point/withdraw")
-    public String showAllWithdrawRequest(Model model) {
+    public String showAllWithdrawRequest(Model model, HttpSession session) {
+        if(!isAdmin(session)){
+            return "redirect:/admin/login";
+        }
 
         List<PointLogWithBankInfo> logWithBankInfos = pointAdminService.getPointLogWithBankInfo();
 
@@ -62,24 +74,35 @@ public class PointAdminController {
     }
 
     @PostMapping("/admin/point/withdraw/waiting/{pointLogId}")
-    public String changeWithdrawLogToWaiting(@PathVariable Long pointLogId) {
-
+    public String changeWithdrawLogToWaiting(@PathVariable Long pointLogId, HttpSession session) {
+        if(!isAdmin(session)){
+            return "redirect:/admin/login";
+        }
         pointAdminService.changeWithdrawLogToWaiting(pointLogId);
         return "redirect:/admin/point/withdraw";
     }
 
     @PostMapping("/admin/point/withdraw/complete/{pointLogId}")
-    public String changeWithdrawLogToCompleteAndEarn(@PathVariable Long pointLogId) {
-
+    public String changeWithdrawLogToCompleteAndEarn(@PathVariable Long pointLogId, HttpSession session) {
+        if(!isAdmin(session)){
+            return "redirect:/admin/login";
+        }
         pointAdminService.changeWithdrawLogToComplete(pointLogId);
         return "redirect:/admin/point/withdraw";
     }
 
     @PostMapping("/admin/point/withdraw/fail/{pointLogId}")
-    public String changeWithdrawLogToFail(@PathVariable Long pointLogId) {
-
+    public String changeWithdrawLogToFail(@PathVariable Long pointLogId, HttpSession session) {
+        if(!isAdmin(session)){
+            return "redirect:/admin/login";
+        }
         pointAdminService.changeWithdrawLogToFail(pointLogId);
         return "redirect:/admin/point/withdraw";
+    }
+
+    private boolean isAdmin(HttpSession session) {
+        String role = (String) session.getAttribute("role");
+        return "ADMIN".equals(role);
     }
 
 }
