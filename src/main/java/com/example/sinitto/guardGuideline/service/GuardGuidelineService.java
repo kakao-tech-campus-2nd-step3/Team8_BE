5a5fcac7b4941920ded7ee2b129fb8016306871c
gitjiho <jiho99322@gmail.com>
2024-11-13 00:40:23 +0900

    feat: getAssignedMemberId가 null일때 nullpointException 방지

--- src/main/java/com/example/sinitto/guardGuideline/service/GuardGuidelineService.java
+++ src/main/java/com/example/sinitto/guardGuideline/service/GuardGuidelineService.java
@@ -61,7 +61,7 @@ public class GuardGuidelineService {
         Callback callback = callbackRepository.findById(callbackId)
                 .orElseThrow(() -> new NotFoundException("존재하지 않는 콜백입니다"));
 
-        if (!callback.getStatus().equals(Callback.Status.WAITING.name()) && !callback.getAssignedMemberId().equals(memberId)) {
+        if (!callback.getStatus().equals(Callback.Status.WAITING.name()) && (callback.getAssignedMemberId() == null || !callback.getAssignedMemberId().equals(memberId))) {
             throw new BadRequestException("해당 콜백은 대기 상태가 아니고, 배정된 시니또가 아닙니다.");
         }
 
